fa4003b03e31c21baf4f75655821d304e8462a51
knoxknox <alexandr_y@mail.ru>
2018-03-03 17:48:24 +0200

    Added CS-168 project (p2)

---
+++ CS-168/server/server_evented.rb
@@ -0,0 +1,94 @@
+require 'socket'
+require './lib/evented'
+
+Thread.abort_on_exception = true
+
+puts "Starting server on port 2000 with pid #{Process.pid}"
+
+server = TCPServer.open(2000)
+
+$client_handlers = {}
+$messages = []
+
+def create_client_handler(nickname, socket)
+  Fiber.new do
+    last_write = Time.now
+
+    loop do
+      state = Fiber.yield
+
+      if state == :readable
+        # Read a message from the socket
+        incoming = read_line_from(socket)
+
+        # If the message is nil the client disconnected
+        if incoming.nil?
+          puts "Disconnected #{nickname}"
+          # Remove the client from storage
+          $client_handlers.delete(socket)
+          # Exit fiber by breaking the loop
+          break
+        end
+
+        # All good, add it to the list to write
+        $messages.push(
+          :time => Time.now,
+          :nickname => nickname,
+          :text => incoming
+        )
+      elsif state == :writable
+        # Write messages to the socket
+        get_messages_to_send(last_write, nickname, $messages).each do |message|
+          socket.puts "#{message[:nickname]}: #{message[:text]}"
+        end
+
+        last_write = Time.now
+      end
+    end
+  end
+end
+
+# Our event loop
+loop do
+  # Step 1: See if we have any new incoming connections
+  begin
+    socket = server.accept_nonblock
+    nickname = socket.gets.chomp
+    $client_handlers[socket] = create_client_handler(nickname, socket)
+    puts "Accepted connection from #{nickname}"
+  rescue IO::WaitReadable, Errno::EINTR
+    # No new incoming connections at the moment
+  end
+
+  # Step 2: Ask the OS to inform us when a connection is ready, wait for 10ms for this to happen
+  # A "real" event loop system would register interest instead of calling this every tick
+  readable, writable = IO.select(
+    $client_handlers.keys,
+    $client_handlers.keys,
+    $client_handlers.keys,
+    0.01
+  )
+
+  # Step 3: See if any of our connections are readable and trigger the client
+  if readable
+    readable.each do |ready_socket|
+      # Get the client from storage
+      client = $client_handlers[ready_socket]
+
+      client.resume(:readable)
+    end
+  end
+
+  # Step 4: See if any of our connections are writable and trigger the client
+  if writable
+    writable.each do |ready_socket|
+      # Get the client from storage
+      client = $client_handlers[ready_socket]
+      next unless client
+
+      client.resume(:writable)
+    end
+  end
+
+  # Done! Onto the next tick of the event loop
+end
---
+++ CS-168/server/server_processes.rb
@@ -0,0 +1,42 @@
+require 'socket'
+require './lib/processes'
+
+Thread.abort_on_exception = true
+
+puts "Starting server on port 2000 with pid #{Process.pid}"
+
+server = TCPServer.open(2000)
+
+client_writers = []
+master_reader, master_writer = IO.pipe
+
+write_incoming_messages_to_child_processes(master_reader, client_writers)
+
+# Run a loop that waits for incoming connections to the server and
+# forks a child process for every connection.
+loop do
+  while socket = server.accept
+
+    # Create a client reader and writer so that the master process can
+    # write messages back to us
+    client_reader, client_writer = IO.pipe
+
+    # Put the client writer on the list of writers so the master process can write to them
+    client_writers.push(client_writer)
+
+    # Fork child process, everything in the fork block only runs in the child process
+    fork do
+      nickname = read_line_from(socket)
+      puts "#{Process.pid}: Accepted connection from #{nickname}"
+
+      write_incoming_messages_to_client(nickname, client_reader, socket)
+
+      # Read incoming messages from the client.
+      while incoming = read_line_from(socket)
+        master_writer.puts "#{nickname}: #{incoming}"
+      end
+
+      puts "#{Process.pid}: Disconnected #{nickname}"
+    end
+  end
+end
